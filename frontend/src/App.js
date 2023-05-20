f911dc42e64c89213e5cba329a5bf03d5e648ce7
Trisha-tech <1234trishasahu@gmail.com>
2023-05-20 05:41:14 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      Welcome to The Sparks Foundation Internship Task: Basic Banking System !!!    Made by Trisha Sahu
+    </div>
+  );
+}
+
+export default App;
